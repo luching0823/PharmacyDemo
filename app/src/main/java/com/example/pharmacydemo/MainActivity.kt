07c50dce984be134ff9d52fdda4d7234fc3f2076
pulin.liao <pulin.liao@55688.com.tw>
2022-12-22 16:55:14 +0800

    parse json data with Gson

--- app/src/main/java/com/example/pharmacydemo/MainActivity.kt
+++ app/src/main/java/com/example/pharmacydemo/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.pharmacydemo.databinding.ActivityMainBinding
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
 import okhttp3.*
 import okhttp3.logging.HttpLoggingInterceptor
 import org.json.JSONArray
@@ -56,23 +58,29 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response) {
 
                 val pharmaciesData = response.body?.string()
+                val pharmacyInfo = Gson().fromJson(pharmaciesData, PharmacyInfo::class.java)
 
-                // 整包字串資料轉成 JSONObject 格式
-                val jsonObj = JSONObject(pharmaciesData)
-
-                // features 是一個陣列，因此需要轉換成 JSON Array
-                val featuresArray = JSONArray(jsonObj.getString("features"))
-
-                // 藥局名稱
-                // 使用 String 處理串接文字，當資料很多時很容易造成 OOM 記憶體不足，建議使用 StringBuilder
                 val pharmacyNames = StringBuilder()
-                for (features in 0 until featuresArray.length()) {
-                    val properties = featuresArray.getJSONObject(features).getString("properties")
-                    val propertiesObj = JSONObject(properties)
-
-                    // 將每次獲取到的藥局名稱，多加跳行符號，存到變數中
-                    pharmacyNames.append(propertiesObj.getString("name") + "\n")
+                for (feature in pharmacyInfo.features) {
+                    pharmacyNames.append(feature.property.name + "\n")
                 }
+
+//                // 整包字串資料轉成 JSONObject 格式
+//                val jsonObj = JSONObject(pharmaciesData)
+//
+//                // features 是一個陣列，因此需要轉換成 JSON Array
+//                val featuresArray = JSONArray(jsonObj.getString("features"))
+//
+//                // 藥局名稱
+//                // 使用 String 處理串接文字，當資料很多時很容易造成 OOM 記憶體不足，建議使用 StringBuilder
+//                val pharmacyNames = StringBuilder()
+//                for (features in 0 until featuresArray.length()) {
+//                    val properties = featuresArray.getJSONObject(features).getString("properties")
+//                    val propertiesObj = JSONObject(properties)
+//
+//                    // 將每次獲取到的藥局名稱，多加跳行符號，存到變數中
+//                    pharmacyNames.append(propertiesObj.getString("name") + "\n")
+//                }
 
                 runOnUiThread {
                     binding.tvPharmaciesData.text = pharmacyNames
@@ -82,3 +90,18 @@ class MainActivity : AppCompatActivity() {
         })
     }
 }
+
+class PharmacyInfo(
+    @SerializedName("features")
+    val features: List<Feature>
+)
+
+class Feature(
+    @SerializedName("properties")
+    val property: Property
+)
+
+class Property(
+    @SerializedName("name")
+    val name: String
+)
